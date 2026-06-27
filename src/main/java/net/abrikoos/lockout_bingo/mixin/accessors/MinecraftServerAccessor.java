4ecac4c5d8f020318ce10af5b22ccd2a0a1f3f23
Abrikoos <45069148+Triple1312@users.noreply.github.com>
2026-06-27 14:53:43 +0200

    world accessor

---
+++ src/main/java/net/abrikoos/lockout_bingo/mixin/accessors/MinecraftServerAccessor.java
@@ -0,0 +1,25 @@
+package net.abrikoos.lockout_bingo.mixin.accessors;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+import java.util.concurrent.Executor;
+
+@Mixin(MinecraftServer.class)
+public interface MinecraftServerAccessor {
+
+    @Accessor("session")
+    LevelStorage.Session getSession();
+
+    @Accessor("workerExecutor")
+    Executor getWorkerExecutor();
+
+    @Accessor("worlds")
+    Map<RegistryKey<World>, ServerWorld> getWorlds();
+}
